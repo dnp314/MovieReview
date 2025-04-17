bb519275f496bd1c4da0ef02d417e542269ac207
Divyesh Parikh <dcparikh360@gmail.com>
2025-04-17 23:06:25 +0530

    Complete

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './App.jsx'
+import App from './App.jsx'
 import StarRating from './StarRating.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       messages={["Bad","Verybad","Ok","Nice","Muuuah"]}
       defaultRating={3}
-    />
+    /> */}
   </StrictMode>,
 )
